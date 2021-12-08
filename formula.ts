acf2d47e651bbb7a5d8afd856131a7f9fb43563f
agvivadigital <agenciavivadigital@gmail.com>
2021-12-08 11:11:10 -0300

    feat(web): started the front-end project

--- formula.ts
+++
@@ -1,7 +0,0 @@
-const result = (13.88 / 100 + 1) ** 1 / 252 - 1
-
-const tcdi = result.toFixed(8)
-
-const tcdiAcumulado = 1 + (tcdi * 103.5) / 100
-
-console.log(tcdiAcumulado)
---
+++ packages/web/webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  module: {
+    rules: [
+      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              query: {
+                name:'assets/images/[hash]-[name].[ext]'
+              }
+            }
+          },
+        ]
+      },
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true,
+    hot: true,
+    port: 4000
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    isDevelopment && new ReactRefreshWebpackPlugin()
+  ].filter(Boolean)
+}
